cc75e9af1df56fbade287e09f78a6cde5c810963
Mikhail <shkrv29@gmail.com>
2026-03-14 16:45:52 +0300

    Create admin.php

---
+++ config/admin.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'username' => 'admin',
+    'password' => '123',
+    // Можно использовать хэш вместо пароля:
+    // 'password_hash' => password_hash('change_me', PASSWORD_DEFAULT),
+];
